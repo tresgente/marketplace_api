4ce54c97160fe602a6666d27710b380b1e6fd9ab
BHUSHAN RAMTEKE <bhushan7668@gmail.com>
2024-07-15 00:44:17 +0530

    post user creating api working.

--- app/controllers/api/v1/application_controller.rb
+++ app/controllers/api/v1/application_controller.rb
@@ -1,14 +1,19 @@
-class ApplicationController < ActionController::API
-  def authenticate_user
-    header = request.headers['Authorization']
-    token = header.split(' ').last if header
-    decoded = JwtService.decode(token)
-    @current_user = User.find(decoded['user_id']) if decoded
-  rescue ActiveRecord::RecordNotFound
-    render json: { error: 'Unauthorized' }, status: :unauthorized
-  end
-
-  def current_user
-    @current_user
+module Api
+  module V1
+    class ApplicationController < ActionController::API
+      def authenticate_user
+        header = request.headers['Authorization']
+        token = header.split(' ').last if header
+        decoded = JwtService.decode(token)
+        @current_user = User.find(decoded['user_id']) if decoded
+      rescue ActiveRecord::RecordNotFound
+        render json: { error: 'Unauthorized' }, status: :unauthorized
+      end
+    
+      def current_user
+        @current_user
+      end
+      
+    end
   end
 end
--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,16 @@
 module Api
   module V1
     class UsersController < ApplicationController
+      def index
+        users = User.all
+        render json: users
+      end
+
+      def show
+        user = User.find(params[:id])
+        render json: user
+      end
+
       def create
         user = User.new(user_params)
         if user.save
@@ -8,6 +18,21 @@ module Api
         else
           render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
         end
+      end
+
+      def update
+        user = User.find(params[:id])
+        if user.update(user_params)
+          render json: user
+        else
+          render json: user.errors, status: :unprocessable_entity
+        end
+      end
+
+      def destroy
+        user = User.find(params[:id])
+        user.destroy
+        head
       end
 
       private
